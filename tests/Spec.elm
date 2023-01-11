57c03de0b548b3b28545ed81279b368d56769e3c
hadrienmp <github@hadrienmp.fr>
2023-01-11 17:28:21 +0100

    Init

--- tests/Spec.elm
+++ tests/Spec.elm
@@ -1,47 +1,14 @@
 module Spec exposing (suite)
 
-import Board
-import Decision exposing (..)
 import Expect
-import Player exposing (Player(..))
 import Test exposing (Test, describe, test)
 
 
 suite : Test
 suite =
-    describe "Tic Tac Toe"
-        [ test "X plays first" <|
+    describe "asfkiasf"
+        [ test "Hihi" <|
             \_ ->
-                Board.init
-                    |> Board.decide
-                    |> Expect.equal (Next X)
-        , test "O plays second" <|
-            \_ ->
-                Board.init
-                    -- Lisibilité plutôt que robustesse
-                    |> Board.play { player = X, position = { x = 0, y = 0 } }
-                    |> Board.decide
-                    |> Expect.equal (Next O)
-        , test "Players take turns" <|
-            \_ ->
-                Board.init
-                    -- Lisibilité plutôt que robustesse
-                    |> Board.play { player = X, position = { x = 0, y = 0 } }
-                    |> Board.play { player = O, position = { x = 0, y = 2 } }
-                    |> Board.decide
-                    |> Expect.equal (Next X)
-        , test "X wins" <|
-            \_ ->
-                Board.init
-                    -- Lisibilité plutôt que robustesse
-                    |> Board.play { player = X, position = { x = 0, y = 0 } }
-                    |> Board.play { player = O, position = { x = 0, y = 2 } }
-                    |> Board.play { player = X, position = { x = 1, y = 0 } }
-                    |> Board.play { player = O, position = { x = 0, y = 1 } }
-                    |> Board.play { player = X, position = { x = 2, y = 0 } }
-                    -- |   | O | O |
-                    -- |   |   |   |
-                    -- | X | X | X |
-                    |> Board.decide
-                    |> Expect.equal (WonBy X)
+                "Haha"
+                    |> Expect.equal "Haha"
         ]
