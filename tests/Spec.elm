a9110d64ff9814ee7bc2ca1676b603ebfc304766
hadrienmp <github@hadrienmp.fr>
2023-01-17 21:16:19 +0100

    ✨ Run

--- tests/Spec.elm
+++ tests/Spec.elm
@@ -1,6 +1,8 @@
 module Spec exposing (suite)
 
+import Css exposing (noHistoricalLigatures)
 import Expect
+import Html exposing (th)
 import Test exposing (Test, describe)
 
 
@@ -25,9 +27,140 @@ import Test exposing (Test, describe)
 
 suite : Test
 suite =
-    describe "Todo rename"
-        [ test "todo rename too" (Expect.equal "Haha" "Haha")
+    describe "Tic tac toe"
+        [ test "X plays first"
+            (emptyGrid
+                |> decide
+                |> Expect.equal (Next X)
+            )
+        , test "O plays second"
+            (emptyGrid
+                |> play X ( 1, 1 )
+                |> decide
+                |> Expect.equal (Next O)
+            )
+        , test "Players take turns"
+            (emptyGrid
+                |> play X ( 1, 1 )
+                |> play O ( 0, 0 )
+                |> play X ( 0, 1 )
+                |> decide
+                |> Expect.equal (Next O)
+            )
+        , test "X wins on first line"
+            --    |   |
+            --  O | O |
+            --  X | X | X
+            (emptyGrid
+                |> play X ( 0, 0 )
+                |> play O ( 0, 1 )
+                |> play X ( 1, 0 )
+                |> play O ( 1, 1 )
+                |> play X ( 2, 0 )
+                |> decide
+                |> Expect.equal (Win X)
+            )
+        , test "X wins on first line from right to left"
+            --    |   |
+            --  O | O |
+            --  X | X | X
+            (emptyGrid
+                |> play X ( 2, 0 )
+                |> play O ( 0, 1 )
+                |> play X ( 1, 0 )
+                |> play O ( 1, 1 )
+                |> play X ( 0, 0 )
+                |> decide
+                |> Expect.equal (Win X)
+            )
         ]
+
+
+type alias Position =
+    { x : Int, y : Int }
+
+
+type alias Move =
+    { player : Player
+    , position : Position
+    }
+
+
+
+-- toto (tata titi)
+-- toto <| tata titi
+-- tata titi |> toto
+-- Typescript
+-- const play = (player: Player, position: [Int, Int], previousGrid: Grid): Grid
+
+
+play : Player -> ( Int, Int ) -> Grid -> Grid
+play player ( x, y ) grid =
+    { player = player, position = { x = x, y = y } } :: grid
+
+
+type alias Grid =
+    List Move
+
+
+emptyGrid : Grid
+emptyGrid =
+    []
+
+
+decide : Grid -> Decision
+decide grid =
+    case findWinner grid of
+        Just player ->
+            Win player
+
+        Nothing ->
+            Next <| findNextPlayer grid
+
+
+findNextPlayer : Grid -> Player
+findNextPlayer grid =
+    case grid of
+        { player } :: _ ->
+            switch player
+
+        [] ->
+            X
+
+
+findWinner : Grid -> Maybe Player
+findWinner grid =
+    if
+        3
+            == (grid
+                    |> List.filter (\{ player } -> player == X)
+                    |> List.length
+               )
+    then
+        Just X
+
+    else
+        Nothing
+
+
+switch : Player -> Player
+switch player =
+    case player of
+        X ->
+            O
+
+        O ->
+            X
+
+
+type Player
+    = X
+    | O
+
+
+type Decision
+    = Next Player
+    | Win Player
 
 
 test : String -> Expect.Expectation -> Test
