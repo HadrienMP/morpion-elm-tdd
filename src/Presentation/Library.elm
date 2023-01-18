9390d0fbd511c440678675f6a219b363bf4de2ce
hadrienmp <github@hadrienmp.fr>
2023-01-18 16:37:12 +0100

    ⚰️ remove comments

--- src/Presentation/Library.elm
+++ src/Presentation/Library.elm
@@ -1,7 +1,6 @@
 module Presentation.Library exposing (..)
 
 import Browser.Events
-import Css
 import Dict
 import Element exposing (Element)
 import Element.Background
@@ -105,32 +104,6 @@ view context model =
         )
 
 
-
--- Html.div
---     [ Attr.css
---         [ Css.position Css.relative
---         , Css.height <| Css.pct 100
---         ]
---     ]
---     [ model.slides
---         |> List.indexedMap Tuple.pair
---         |> Dict.fromList
---         |> Dict.get model.currentSlide
---         |> Maybe.map (slide context)
---         |> Maybe.withDefault (Html.h1 [] [ Html.text <| "What slide now ? " ++ String.fromInt model.currentSlide ])
---     , Html.div
---         [ Attr.css
---             [ Css.position Css.absolute
---             , Css.right <| Css.rem 4
---             , Css.bottom <| Css.rem 4
---             ]
---         ]
---         [ Html.button [ Evts.onClick Previous ] [ Html.text "<" ]
---         , Html.button [ Evts.onClick Next ] [ Html.text ">" ]
---         ]
---     ]
-
-
 slide : context -> Slide context msg -> Element msg
 slide context { content, background } =
     Element.el
@@ -168,47 +141,3 @@ transparentBackground background context =
         , Element.height Element.fill
         ]
         Element.none
-
-
-
--- Html.div
---     [ Attr.class "slide"
---     , Attr.css
---         [ Css.position Css.relative
---         , Css.height <| Css.pct 100
---         ]
---     ]
---     [ Html.div
---         [ Attr.css
---             [ case background of
---                 Just it ->
---                     Css.backgroundImage <| Css.url <| it context
---                 Nothing ->
---                     Css.backgroundImage Css.none
---             , Css.backgroundSize <| Css.cover
---             , Css.backgroundPosition Css.right
---             , Css.position Css.absolute
---             , Css.top Css.zero
---             , Css.bottom Css.zero
---             , Css.left Css.zero
---             , Css.right Css.zero
---             , Css.opacity <| Css.num 0.3
---             ]
---         ]
---         []
---     , Html.div
---         [ Attr.css
---             [ Css.position Css.absolute
---             , Css.zIndex <| Css.int 10
---             , Css.maxHeight <| Css.pct 98
---             , Css.maxWidth <| Css.pct 98
---             , Css.width <| Css.px 960
---             , Css.height <| Css.px 700
---             , Css.top <| Css.pct 50
---             , Css.left <| Css.vw 50
---             , Css.transform <| Css.translate2 (Css.pct -50) (Css.pct -50)
---             , Css.overflow Css.hidden
---             ]
---         ]
---         [ content context ]
---     ]
