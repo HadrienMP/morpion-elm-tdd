5f21f8f9612438c3e421b237b5ff2872e2219651
hadrienmp <github@hadrienmp.fr>
2023-01-24 11:48:12 +0100

    💄 Improve the theme a bit

--- src/Pages/Game.elm
+++ src/Pages/Game.elm
@@ -5,12 +5,13 @@ import Domain.Grid as Grid
 import Domain.Player as Player
 import Domain.Position as Position
 import Domain.TicTacToe
-import Element exposing (Element, centerY, htmlAttribute)
+import Element exposing (Element, centerY)
+import Element.Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
-import Html.Attributes
 import Types exposing (Size)
+import UI.Colors
 import UI.Space
 
 
@@ -37,6 +38,7 @@ init =
 
 type Msg
     = PlayAt Position.Position
+    | Restart
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -49,6 +51,9 @@ update msg model =
             in
             ( { grid = next, lastDecision = Domain.TicTacToe.decide next }, Cmd.none )
 
+        ( Restart, _ ) ->
+            ( init, Cmd.none )
+
         _ ->
             ( model, Cmd.none )
 
@@ -59,28 +64,59 @@ update msg model =
 
 view : Types.Images -> Size -> Model -> Element Msg
 view images availableSize model =
-    Element.column
-        [ Element.centerX
-        , Element.centerY
-        , Element.spacing UI.Space.m
+    Element.el
+        [ Element.inFront <|
+            case model.lastDecision of
+                Domain.Decision.Next _ ->
+                    Element.none
+
+                _ ->
+                    Element.el
+                        [ Element.width Element.fill
+                        , Element.height Element.fill
+                        , Element.Background.color <| UI.Colors.withAlpha 0.6 UI.Colors.background
+                        ]
+                    <|
+                        Element.column
+                            [ Element.centerX
+                            , Element.paddingXY 0 UI.Space.m
+                            , Element.centerY
+                            , Element.rotate -0.1
+                            , Element.spacing UI.Space.m
+                            ]
+                            [ displayDecision availableSize model
+                            , Input.button
+                                [ Element.Background.color UI.Colors.accent
+                                , Font.color UI.Colors.onAccent
+                                , Element.padding UI.Space.m
+                                , Element.centerX
+                                , Border.width 1
+                                , Font.size <| minDimension availableSize // 14
+                                ]
+                                { onPress = Just Restart, label = Element.text "Play again !" }
+                            ]
+        , Element.width Element.fill
+        , Element.height Element.fill
         ]
-        [ displayDecision availableSize model
-        , displayGrid images availableSize model
-        ]
+    <|
+        displayGrid images availableSize model
 
 
 displayDecision : Size -> Model -> Element Msg
 displayDecision availableSize model =
-    Element.el [ Font.size <| minDimension availableSize // 9 ] <|
-        Element.text <|
-            case model.lastDecision of
-                Domain.Decision.Next player ->
-                    Player.toString player ++ " is next"
+    case model.lastDecision of
+        Domain.Decision.Next _ ->
+            Element.none
 
-                Domain.Decision.Win player ->
-                    Player.toString player ++ " wins !"
+        Domain.Decision.Win player ->
+            Element.el [ Font.size <| minDimension availableSize // 9 ] <|
+                Element.text <|
+                    Player.toString player
+                        ++ " wins !"
 
-                Domain.Decision.Draw ->
+        Domain.Decision.Draw ->
+            Element.el [ Font.size <| minDimension availableSize // 9 ] <|
+                Element.text <|
                     "It's a draw !"
 
 
@@ -91,10 +127,6 @@ minDimension size =
 
 displayGrid : Types.Images -> Size -> Model -> Element Msg
 displayGrid images availableSize model =
-    let
-        cellSize =
-            minDimension availableSize // 5
-    in
     Element.column
         [ Element.centerX
         , Element.centerY
@@ -107,38 +139,78 @@ displayGrid images availableSize model =
                         (row
                             |> List.map
                                 (\position ->
-                                    Input.button
-                                        [ Element.width <| Element.px <| cellSize
-                                        , Element.height <| Element.px <| cellSize
-                                        , Border.width 1
-                                        ]
-                                        { onPress = Just <| PlayAt position
-                                        , label =
-                                            case
-                                                Grid.findMoveAt position model.grid
-                                            of
-                                                Just { player } ->
-                                                    Element.image
-                                                        [ Element.width <| Element.px <| cellSize * 3 // 4
-                                                        , Element.centerX
-                                                        , centerY
-                                                        ]
-                                                    <|
-                                                        case player of
-                                                            Player.X ->
-                                                                { src = images.finn
-                                                                , description = "Finn from adventure time"
-                                                                }
-
-                                                            Player.O ->
-                                                                { src = images.jake
-                                                                , description = "Jake from adventure time"
-                                                                }
-
-                                                Nothing ->
-                                                    Element.none
-                                        }
+                                    displayField availableSize position model images
                                 )
                         )
                 )
         )
+
+
+displayField : Size -> Position.Position -> Model -> Types.Images -> Element Msg
+displayField availableSize position model images =
+    let
+        cellSize =
+            minDimension availableSize // 5
+
+        borderWidth =
+            max 1 <| cellSize // 90
+
+        sides =
+            { top =
+                if position.y == 2 then
+                    0
+
+                else
+                    borderWidth
+            , bottom =
+                if position.y == 0 then
+                    0
+
+                else
+                    borderWidth
+            , left =
+                if position.x == 0 then
+                    0
+
+                else
+                    borderWidth
+            , right =
+                if position.x == 2 then
+                    0
+
+                else
+                    borderWidth
+            }
+    in
+    Input.button
+        [ Element.width <| Element.px <| cellSize
+        , Element.height <| Element.px <| cellSize
+        , Border.widthEach sides
+        , Border.color <| UI.Colors.darken 10 UI.Colors.accent
+        ]
+        { onPress = Just <| PlayAt position
+        , label =
+            case
+                Grid.findMoveAt position model.grid
+            of
+                Just { player } ->
+                    Element.image
+                        [ Element.width <| Element.px <| cellSize * 3 // 5
+                        , Element.centerX
+                        , centerY
+                        ]
+                    <|
+                        case player of
+                            Player.X ->
+                                { src = images.finn
+                                , description = "Finn from adventure time"
+                                }
+
+                            Player.O ->
+                                { src = images.jake
+                                , description = "Jake from adventure time"
+                                }
+
+                Nothing ->
+                    Element.none
+        }
--- src/UI/Colors.elm
+++ src/UI/Colors.elm
@@ -1,8 +1,12 @@
-module UI.Colors exposing (accent, background, onBackground)
+module UI.Colors exposing (accent, background, darken, onAccent, onBackground, withAlpha)
 
 import Color
 import Element
 import Element.HexColor exposing (rgbCSSHex)
+
+
+
+-- Palette
 
 
 background : Element.Color
@@ -17,9 +21,35 @@ onBackground =
 
 accent : Element.Color
 accent =
-    rgbCSSHex "#34b2ff"
+    rgbCSSHex "#f2c92f"
+
+
+onAccent : Element.Color
+onAccent =
+    rgbCSSHex "#000"
+
+
+
+-- Functions
 
 
 toElement : Color.Color -> Element.Color
 toElement =
     Color.toRgba >> Element.fromRgb
+
+
+darken : Int -> Element.Color -> Element.Color
+darken factor =
+    Element.toRgb
+        >> Color.fromRgba
+        >> Color.toHsla
+        >> (\color -> { color | lightness = color.lightness * (1 - toFloat factor / 100) })
+        >> Color.fromHsla
+        >> toElement
+
+
+withAlpha : Float -> Element.Color -> Element.Color
+withAlpha alpha =
+    Element.toRgb
+        >> (\color -> { color | alpha = alpha })
+        >> Element.fromRgb
