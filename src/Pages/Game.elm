cff7c58ad4154dde5cb43606a6fa58e78a3e6579
hadrienmp <github@hadrienmp.fr>
2023-02-07 19:38:00 +0100

    remove domain code

--- src/Pages/Game.elm
+++ src/Pages/Game.elm
@@ -1,10 +1,5 @@
 module Pages.Game exposing (Model, Msg(..), init, update, view)
 
-import Domain.Decision
-import Domain.Grid as Grid
-import Domain.Player as Player
-import Domain.Position as Position
-import Domain.TicTacToe
 import Element
 import Element.Background as Background
 import Element.Border as Border
@@ -22,12 +17,12 @@ import UI.Space
 
 
 type alias Model =
-    Domain.TicTacToe.TicTacToe
+    ()
 
 
 init : Model
 init =
-    Domain.TicTacToe.start
+    ()
 
 
 
@@ -35,18 +30,12 @@ init =
 
 
 type Msg
-    = PlayAt Position.Position
-    | Restart
+    = Restart
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg _ =
     case msg of
-        PlayAt position ->
-            ( Domain.TicTacToe.playAt position model
-            , Cmd.none
-            )
-
         Restart ->
             ( init, Cmd.none )
 
@@ -58,37 +47,7 @@ update msg model =
 view : Types.Images -> Size -> Model -> Element.Element Msg
 view images availableSize model =
     Element.el
-        [ Element.inFront <|
-            case model.lastDecision of
-                Domain.Decision.Next _ ->
-                    Element.none
-
-                _ ->
-                    Element.el
-                        [ Element.width Element.fill
-                        , Element.height Element.fill
-                        , Background.color <| UI.Colors.withAlpha 0.6 UI.Colors.background
-                        ]
-                    <|
-                        Element.column
-                            [ Element.centerX
-                            , Element.paddingXY 0 UI.Space.m
-                            , Element.centerY
-                            , Element.rotate -0.1
-                            , Element.spacing UI.Space.m
-                            ]
-                            [ displayDecision availableSize model
-                            , Input.button
-                                [ Background.color UI.Colors.accent
-                                , Font.color UI.Colors.onAccent
-                                , Element.padding UI.Space.m
-                                , Element.centerX
-                                , Border.width 1
-                                , Font.size <| minDimension availableSize // 14
-                                ]
-                                { onPress = Just Restart, label = Element.text "Play again !" }
-                            ]
-        , Element.width Element.fill
+        [ Element.width Element.fill
         , Element.height Element.fill
         ]
     <|
@@ -97,45 +56,8 @@ view images availableSize model =
             , Element.centerY
             , Element.spacing UI.Space.m
             ]
-            [ case model.lastDecision of
-                Domain.Decision.Next player ->
-                    Element.row
-                        [ Font.size <| minDimension availableSize // 10
-                        , Element.centerX
-                        ]
-                        [ playerProperties player images
-                            |> .image
-                            |> Element.image
-                                [ Element.width <|
-                                    Element.px <|
-                                        minDimension availableSize
-                                            // 10
-                                ]
-                        , Element.el [ Element.moveDown 2 ] <| Element.text " is next"
-                        ]
-
-                _ ->
-                    Element.none
-            , displayGrid images availableSize model
+            [ displayGrid images availableSize model
             ]
-
-
-displayDecision : Size -> Model -> Element.Element Msg
-displayDecision availableSize model =
-    case model.lastDecision of
-        Domain.Decision.Next _ ->
-            Element.none
-
-        Domain.Decision.Win player ->
-            Element.el [ Font.size <| minDimension availableSize // 9 ] <|
-                Element.text <|
-                    Player.toString player
-                        ++ " wins !"
-
-        Domain.Decision.Draw ->
-            Element.el [ Font.size <| minDimension availableSize // 9 ] <|
-                Element.text <|
-                    "It's a draw !"
 
 
 minDimension : Size -> Int
@@ -149,7 +71,10 @@ displayGrid images availableSize model =
         [ Element.centerX
         , Element.centerY
         ]
-        (Grid.rows
+        ([ [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]
+         , [ ( 0, 1 ), ( 1, 1 ), ( 2, 1 ) ]
+         , [ ( 0, 2 ), ( 1, 2 ), ( 2, 2 ) ]
+         ]
             |> List.reverse
             |> List.map
                 (\row ->
@@ -164,8 +89,8 @@ displayGrid images availableSize model =
         )
 
 
-displayField : Size -> Position.Position -> Model -> Types.Images -> Element.Element Msg
-displayField availableSize position model images =
+displayField : Size -> ( Int, Int ) -> Model -> Types.Images -> Element.Element Msg
+displayField availableSize ( x, y ) model images =
     let
         cellSize =
             minDimension availableSize
@@ -178,25 +103,25 @@ displayField availableSize position model images =
 
         sides =
             { top =
-                if position.y == 2 then
+                if y == 2 then
                     0
 
                 else
                     borderWidth
             , bottom =
-                if position.y == 0 then
+                if y == 0 then
                     0
 
                 else
                     borderWidth
             , left =
-                if position.x == 0 then
+                if x == 0 then
                     0
 
                 else
                     borderWidth
             , right =
-                if position.x == 2 then
+                if x == 2 then
                     0
 
                 else
@@ -208,57 +133,15 @@ displayField availableSize position model images =
         , Element.height <| Element.px <| cellSize
         , Border.widthEach sides
         , Border.color <| UI.Colors.darken 10 UI.Colors.accent
+        , Font.size 30
+        , Font.center
         , Element.htmlAttribute <|
             Html.Attributes.property "style" <|
                 Json.string <|
-                    case model.lastDecision of
-                        Domain.Decision.Next player ->
-                            "cursor: url('"
-                                ++ (playerProperties player images |> .cursor)
-                                ++ "'), auto"
-
-                        _ ->
-                            "cursor: pointer"
+                    "cursor: url('"
+                        ++ images.finnCursor
+                        ++ "'), auto"
         ]
-        { onPress = Just <| PlayAt position
-        , label =
-            case
-                Grid.findMoveAt position model.grid
-            of
-                Just { player } ->
-                    Element.image
-                        [ Element.width <| Element.px <| cellSize * 3 // 5
-                        , Element.centerX
-                        , Element.centerY
-                        ]
-                    <|
-                        .image <|
-                            playerProperties player images
-
-                Nothing ->
-                    Element.none
+        { onPress = Nothing
+        , label = Element.text (String.fromInt x ++ "," ++ String.fromInt y)
         }
-
-
-type alias PlayerProperties =
-    { image : { src : String, description : String }, cursor : String }
-
-
-playerProperties : Player.Player -> Types.Images -> PlayerProperties
-playerProperties player images =
-    case player of
-        Player.X ->
-            { image =
-                { src = images.finn
-                , description = "Finn"
-                }
-            , cursor = images.finnCursor
-            }
-
-        Player.O ->
-            { image =
-                { src = images.jake
-                , description = "Jake"
-                }
-            , cursor = images.jakeCursor
-            }
