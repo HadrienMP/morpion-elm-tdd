afbb38ab4dace364573bd93d3cbf17118637366b
Alexis Lozano <alexis.lozano@360learning.com>
2023-02-12 12:18:30 +0100

    on s'est bien amusés

--- tests/Spec.elm
+++ tests/Spec.elm
@@ -28,7 +28,135 @@ import Test exposing (Test, describe)
 
 suite : Test
 suite =
-    describe "Coucou change moi"
-        [ test "Et moi !"
-            (Expect.equal "oui" "oui")
+    describe "Morpion"
+        [ test "X plays first"
+            (emptyGame
+                |> expectNotWin X
+            )
+        , test "O plays second"
+            (emptyGame
+                |> play TopLeft
+                |> expectNotWin O
+            )
+        , test "X plays third"
+            (emptyGame
+                |> play TopLeft
+                |> play TopMiddle
+                |> expectNotWin X
+            )
+        , test "X wins"
+            (emptyGame
+                |> play TopLeft
+                |> play MiddleLeft
+                |> play TopMiddle
+                |> play MiddleMiddle
+                |> play TopRight
+                |> play MiddleRight
+                |> play BottomLeft
+                |> play BottomMiddle
+                |> play BottomRight
+                |> Expect.equal (Win X)
+            )
+        , test "Cannot play two times at the same position"
+            (emptyGame
+                |> play TopLeft
+                |> play TopLeft
+                |> expectNotWin O
+            )
+        , test "Cannot play two times at the same position on last move"
+            (emptyGame
+                |> play TopLeft
+                |> play MiddleLeft
+                |> play TopMiddle
+                |> play MiddleMiddle
+                |> play BottomRight
+                |> play MiddleRight
+                |> play BottomLeft
+                |> play BottomMiddle
+                |> play TopLeft
+                |> expectNotWin X
+            )
         ]
+
+
+expectNotWin : Player -> Game -> Expect.Expectation
+expectNotWin expectedPlayer status =
+    case status of
+        Win _ ->
+            Expect.fail "Got Win, expected Not Win"
+
+        NotWin grid ->
+            Expect.equal (nextPlayer grid) expectedPlayer
+
+
+play : Position -> Game -> Game
+play target status =
+    case status of
+        Win _ ->
+            status
+
+        NotWin grid ->
+            if List.member target (grid |> List.map .position) then
+                NotWin grid
+
+            else if
+                (grid
+                    |> List.filter (\{ player, position } -> player == X && List.member position [ TopLeft, TopMiddle, TopRight ])
+                    |> List.length
+                )
+                    == 3
+            then
+                Win X
+
+            else
+                NotWin <| { position = target, player = nextPlayer grid } :: grid
+
+
+type Player
+    = X
+    | O
+
+
+type Position
+    = TopLeft
+    | TopMiddle
+    | TopRight
+    | MiddleLeft
+    | MiddleMiddle
+    | MiddleRight
+    | BottomLeft
+    | BottomMiddle
+    | BottomRight
+
+
+type alias Move =
+    { position : Position, player : Player }
+
+
+type alias Grid =
+    List Move
+
+
+nextPlayer : Grid -> Player
+nextPlayer grid =
+    case grid of
+        [] ->
+            X
+
+        { player } :: _ ->
+            case player of
+                X ->
+                    O
+
+                O ->
+                    X
+
+
+emptyGame : Game
+emptyGame =
+    NotWin []
+
+
+type Game
+    = Win Player
+    | NotWin Grid
